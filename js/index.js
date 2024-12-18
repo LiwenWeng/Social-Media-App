5d3a03c6f6f3b90550602b423ab19e56ebf0e0bf
Leo <apollycn@gmail.com>
2024-12-18 11:45:05 -0500

    feat: slide indicator functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 import { getAlbum, getPhotosForAlbum, getUser } from "./data.mjs";
 
 const scrollContainer = document.querySelector("#scroll-container");
+const dotContainer = document.querySelector("#dot-container");
+
+const data = new Map();
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -9,10 +12,11 @@ function shuffleArray(array) {
     }
 }
 
-function setupSection(id, section) {
+function setupSection(n, id, section) {
     const photosPromise = getPhotosForAlbum(id);
 
     photosPromise.then(photos => {
+        data.set(n, photos.length);
         for (const i in photos) {
             const slide = document.createElement("div");
             slide.classList.add("slide");
@@ -28,7 +32,38 @@ function setupSection(id, section) {
             
             section.appendChild(slide);
         }
+
+        if (n === 0) {
+            for (let j = 0; j < data.get(0); j++) {
+                const dot = document.createElement("div");
+                dot.classList.add("dot");
+                if (j === 0) dot.classList.add("active");
+                dotContainer.appendChild(dot);
+            }
+        }
     });
+
+    let previousSlide = -1;
+    let currentSlide = 0;
+    let previousScrollLeft = 0;
+    section.addEventListener("scroll", () => {
+        let currentScrollLeft = section.scrollLeft;
+        const slideWidth = [...section.children][0].offsetWidth;
+        previousSlide = currentSlide;
+
+        if (currentScrollLeft < previousScrollLeft) {
+            currentSlide = Math.floor(currentScrollLeft / slideWidth);
+        } else if (currentScrollLeft > previousScrollLeft) {
+            currentSlide = Math.ceil(currentScrollLeft / slideWidth);
+        }
+        previousScrollLeft = currentScrollLeft;
+        
+        const children = dotContainer.children;
+        if (previousSlide !== currentSlide) {
+            children[currentSlide].classList.add("active");
+            children[previousSlide].classList.remove("active");
+        }
+    })
 
     section.classList.add("section");
     return section;
@@ -40,21 +75,21 @@ function init() {
 
     for (let i = 0; i < array.length; i++) {
         const section = setupSection(
+            i,
             array[i],
-            document.createElement("div"),
+            document.createElement("div")
         );
 
-        const author = document.createElement("h3");
-        author.classList.add("author");
-
-        scrollContainer.appendChild(author);
         scrollContainer.appendChild(section);
     }
 
+    let previousSection = 0;
+    let currentSection = 0;
     scrollContainer.addEventListener("scroll", () => {
         const scrollPosition = scrollContainer.scrollTop; 
-        const slideWidth = scrollContainer.offsetHeight;
-        const currentSection = Math.floor(scrollPosition / slideWidth) + 1;
+        const slideHeight = scrollContainer.offsetHeight;
+        previousSection = currentSection;
+        currentSection = Math.floor(scrollPosition / slideHeight) + 1;
         
         const albumPromise = getAlbum(array[currentSection - 1]);
         albumPromise.then(album => {
@@ -62,8 +97,19 @@ function init() {
             userPromise.then(user => {
                 document.querySelector(".author").textContent = `Author: ${user.name}`;
             });
-        }); 
+        });
+
+        if (currentSection !== previousSection) {
+            dotContainer.innerHTML = "";
+            if (data.get(currentSection-1) === 1) return;
+            for (let i = 0; i < data.get(currentSection-1); i++) {
+                const dot = document.createElement("div");
+                dot.classList.add("dot");
+                if (i === 0) dot.classList.add("active");
+                dotContainer.appendChild(dot);
+            }
+        }
     });
 }
 
-init()
+init();
